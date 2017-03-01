102715aee9bb19151ac20a80e03af5d0323ecb69
maureenroche <roche.maureen@gmail.com>
2017-03-01 14:09:52 +0100

    fix bug link front/back

--- api/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("HOST", "localhost");
-	define("USER", "root");
-	define("PSWD", "");
-	define("BDD", "plume");
-?>
--- api/Controllers/User/getUserState.php
+++ api/Controllers/User/getUserState.php
@@ -1,13 +1,9 @@
-<?php 
+<?php
 	header('Access-Control-Allow-Origin:*');
 	header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 	header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 	header('Content-Type: application/json;charset=utf-8');
-
-	include "../../Models/UserModel.php";
-
-	$data = UserModel::getUserState();
-
-  echo json_encode($data);
+	
+	echo "1";
 
 ?>
