85817c2262dd8cc27e892faeea42ac3d79c6c070
hellowizz <heloroussel@hotmail.fr>
2017-03-07 18:08:35 +0100

    Admin as been created

---
+++ src/components/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+
+	<div class="admin">
+
+  </div>
+
+</template>
+
+<script>
+  import {apiRoot} from '../../config/localhost/settings.js'
+  export default {
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
