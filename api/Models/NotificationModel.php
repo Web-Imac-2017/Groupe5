9f958fa3359e4c9bb6828e94f1a9e392b51b1ee5
ProvencalLeGaulois <etienne.juif@gmail.com>
2017-03-07 16:01:34 +0100

    Add NotificationModel

---
+++ api/Models/NotificationModel.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('../../Database.php');
+require_once('../../Models/UserModel.php');
+
+class NotificationModel{
+    
+    public static function addNotification($contenu, $pseudo1, $pseudo2){ /*pseudo 1 = emetteur, pseudo 2 = recepteur*/
+        $bdd = Database::connexionBDD();
+        
+        $id_user1 = UserModel::getUserId($pseudo1);
+        $id_user2 = UserModel::getUserId($pseudo2);
+
+        $req_active = $bdd->prepare("INSERT INTO notification (`ID`, `ID_user1`, `ID_user2`,`contenu`, `date`) VALUES (NULL, :user1, :user2,:contenu, now()); ");
+        $req_active->execute(array(':contenu' => $contenu, ':user1' => $id_user1, ':user2' => $id_user2));
+        $result = $req_active->fetchAll();
+
+        if ($result == FALSE){
+            $data = array("Error", "Error: the request doesn't work.");
+        }
+        else{
+             $data = array(0);
+        }
+        return $data;
+    }
+
+    /*cette fonction renvoie un tableau contenant les notifications classés du + récent au + ancient */
+    public static function getAllNotif($pseudo){
+        $bdd = Database::connexionBDD();
+        $result = [];
+
+        $id_user = UserModel::getUserId($pseudo);
+
+        $req_active = $bdd->prepare("SELECT `ID`, `ID_user2`, `date`, `contenu` as `content` FROM `notification` WHERE `ID_user1` = :user1 ORDER BY `date` ASC;");
+        $req_active->execute(array(':user1' => $id_user));
+        
+        $result = $req_active->fetchAll();
+        
+        /*var_dump($result);*/
+
+        for($i=0; $i < count($result); $i++){
+            $num_id = intval($result[$i]['ID_user2']);
+            $result[$i]['user'] = UserModel::getPseudoById($num_id);
+        }
+       
+        return $result;
+    }
+    
+    public static function deleteNotif($idNotif){
+        $bdd = Database::connexionBDD();
+
+        $req_active = $bdd->prepare('DELETE FROM notification WHERE ID = "'.$idNotif.'"');
+        $req_active->execute();
+        $result = $req_active->fetchAll();
+
+        if ($result == FALSE){
+            $data = array("Error", "Error:  the request doesn't work.");
+        }
+        else{
+             $data = array(0);
+        }
+        return $data;
+    }
+    
+ 
+?>
