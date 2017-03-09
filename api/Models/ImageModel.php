2b787377d2b1ce94146df76055232a59c7fd710d
PaulineLg <paulinedivry@aol.com>
2017-03-09 19:21:12 +0100

    Add ImageModel and uploadImageMEssage controller.

--- api/Models/ImageModel.php
+++ api/Models/ImageModel.php
@@ -5,10 +5,38 @@ require_once('../../Models/UserModel.php');
 
 class ImageModel{
     
-    public static function getImage () {
+    // Enregistre la balise img html pour afficher l'image dans la conversation
+    public static function uploadImageMessage($url, $pseudo, $id_conv) {
+        $bdd = Database::connexionBDD();
+        $id_user = UserModel::getUserId($pseudo);
+        $contenu = "<img alt='Message' src='".$url."'/>";
+        
+        $req_active = $bdd->prepare("INSERT INTO `message`(`ID`, `contenu`, `date`, `id_user`, `id_conversation`) VALUES (NULL,:contenu, now(), :id_user, :id_conv)");
+        $result = $req_active->execute(array(':contenu' => $contenu, ':id_user' => $id_user, ':id_conv' => $id_conv));
+        
+        if($result){
+            $data = $result;
+        }
+        else {
+            $data = array("Error", "The request doesn't work");
+        }
+        
+        return $data;
+    }
+    
+    //Récupère la balise image pour la réafficher dans la conversation
+    public static function getImageMessage () {
         
     }
-
-
+    
+    //Enregistre l'avatar d'un utilisateur
+    public static function uploadAvatar() {
+        
+    }
+    
+    //Va chercher l'avatar d'un utilisateur :
+    public static function getAvatar() {
+        
+    }
 
 }
