75fa5491346469d9ccfe44f71add9445dc3ce52b
hellowizz <heloroussel@hotmail.fr>
2017-03-09 16:04:01 +0100

    Admin page almost finished!

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -4,41 +4,43 @@
 
     <div class="row" id="title">
       <div class="col-sm-12">
-        JE SUIS UNE CASSOS
+        ADMINISTRATION
       </div>
     </div>
 
-    <div class="row acceptation">
-      <div class="col-sm-12">
-
-        <div class="row">
-
-            <div class="col-lg-4">
-              <div class = "carre"> </div> 
-            </div>
-
-            <div class="col-lg-8">
-
-              <div class="name"> NameProfile </div>
-
-              <div class="row">
-
-                <div class="col-lg-6">
-                  <div class = "valid">O</div>
+    <ul>
+      <li v-for="user in users">
+        <div class="row acceptation">
+          <div class="col-sm-12">
+            <div class="row">
+                <div class="col-lg-4">
+                  <img v-bind:src="'/static/avatar/' + user.avatar"> 
                 </div>
 
-                <div class="col-lg-6">
-                  <div class = "refuse">X</div>
+                <div class="col-lg-8">
+
+                  <div class="name"> {{user.pseudo}} </div>
+
+                  <div class="row">
+
+                    <div class="col-lg-6">
+                      <div class = "valid">
+                        <icon name="check"></icon>
+                      </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                      <div class = "refuse">
+                        <icon name="times"></icon>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-
-              </div>
-
             </div>
-
+          </div>
         </div>
-
-      </div>
-    </div>
+      </li>
+    </ul>
 
   </div>
 
@@ -49,79 +51,94 @@
   export default {
     data() {
       return {
+        users : {
+          avatar : '',
+          pseudo : ''
+        }
       }
     },
+    
+    methods: {
+    getAvatarsNotChecked: function() {
+      var _this = this;
+      fetch(apiRoot() + 'Controllers/User/getAvatarsNotChecked.php', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        dataType: 'JSON',
+      }).then(function(response) {
+        return response.json();
+      }).then(function(data){
+        if(data[0] == "Error"){
+          console.log("ERREUR !!");
+        }
+        else {
+          _this.users = data['users'];
+        }
+      });
+      }
+    },
+    created : function(){
+      this.getAvatarsNotChecked();
+    }
   }
 </script>
 
 <style lang="scss">
 
-[class*="col-"] {
-  background-color: lightgreen;
-  border: 2px solid black;
-  border-radius: 6px;
-  line-height: 40px;
-  text-align: center;
-}
-
 .admin{
   width : 70%;
   margin : auto;
-}
-
-#title
-{
-  margin : 5%;
-}
-
-.acceptation
-{
-  margin: 3%;
-  border: 1px solid black;
-
-  .name
+  line-height: 40px;
+  text-align: center;
+  font-size: 20px;
+    
+  #title
   {
-    font-weight: 900; 
+    margin : 5%;
   }
 
-  .valid
+  .acceptation
   {
-    margin : auto;
-    width : 100px;
-    height: 100px;
-    border-radius: 2%;
-    background-color: green;
-  }
+    margin: 3%;
+    padding: 2%;
+    border: 2px solid black;
 
-  .refuse
-  {
-    margin : auto;
-    width : 100px;
-    height: 100px;
-    border-radius: 2%;
-    background-color: red;
-  }
+    .name
+    {
+      font-weight: 900; 
+    }
 
-  .valid:hover
-  {
-    width : 120px;
-    height : 120px;
-  }
+    img
+    {
+      width: 100%;
+      border: 1px solid black;
+    }
 
-  .refuse:hover
-  {
-    width : 120px;
-    height : 120px;
-  }
-}
+    .valid, .refuse 
+    {
+      margin : 15px;
+    }
 
-.carre
-{
-  margin : auto;
-  width : 200px;
-  height: 200px;
-  border-radius: 2%;
-  background-color: pink;
+    .fa-icon
+    {
+      margin : auto;
+      margin : 10%;
+      width : 80px;
+      height : 80px;
+      cursor : pointer;
+    }
+
+    .valid .fa-icon:hover{
+      color : lightgreen;
+    }
+
+    .refuse .fa-icon:hover{
+      color : red;
+    }
+  }
 }
 
 </style>
