1435181a08aace7a7cf3f39e5991e5c50c7c9a98
Adrian Gallet <gallet.adrian@gmail.com>
2017-03-13 18:02:08 +0100

    Add Order by to requetes

--- api/Models/GeneralModel.php
+++ api/Models/GeneralModel.php
@@ -8,7 +8,7 @@ class GeneralModel{
     public static function getAllHobbies(){
         $bdd = Database::connexionBDD();
         
-        $req_get = $bdd->prepare('SELECT Nom as name FROM centre_interet');
+        $req_get = $bdd->prepare('SELECT Nom as name FROM centre_interet ORDER BY Nom');
         $req_get->execute();
         $result = array();
         $result['hobbies'] = $req_get->fetchAll();
@@ -18,7 +18,7 @@ class GeneralModel{
     public static function getAllLanguages(){
         $bdd = Database::connexionBDD();
         
-        $req_get = $bdd->prepare('SELECT Nom as name FROM langue');
+        $req_get = $bdd->prepare('SELECT Nom as name FROM langue ORDER BY Nom');
         $req_get->execute();
         $result = array();
         $result['languages'] = $req_get->fetchAll();
@@ -28,7 +28,7 @@ class GeneralModel{
     public static function getAllCountries(){
         $bdd = Database::connexionBDD();
         
-        $req_get = $bdd->prepare('SELECT nom_pays as name FROM table_pays WHERE code != "00"');
+        $req_get = $bdd->prepare('SELECT nom_pays as name FROM table_pays WHERE code != "00" ORDER BY nom_pays');
         $req_get->execute();
         $result = array();
         $result['countries'] = $req_get->fetchAll();
