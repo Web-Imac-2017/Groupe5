11fc4cc7d8922b0f926d328a3a9cd86b91429548
Aleqsandr <alexandre.reho@gmail.com>
2017-03-15 14:07:09 +0100

    echo json OK + desig notification validated OK

--- api/Controllers/Notification/getAllNotif.php
+++ api/Controllers/Notification/getAllNotif.php
@@ -5,16 +5,14 @@ header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header('Content-Type: application/json;charset=utf-8');
 
-echo '{
-    "notifications" : [
-        {
-            "ID" : "5",
-            "ID_user1" : "3",
-            "date" : "2017-03-06 00:00:00",
-            "content" : "Conversation request from",
-            "user" : "sapristi"
-        }
-    ] 
-}';
+echo '[
+    {
+        "ID" : "5",
+        "ID_user1" : "3",
+        "date" : "2017-03-06 00:00:00",
+        "content" : "would like to talk with you",
+        "user" : "Aleqsandr"
+    }
+]';
 
 ?>
--- src/App.vue
+++ src/App.vue
@@ -256,7 +256,7 @@ export default {
           console.log(data[1]);
         }
         else {
-          //console.log(data);
+          console.log(data);
           _this.notifications = data;
         }
       });
@@ -353,7 +353,13 @@ export default {
 }
 </script>
 
-  <style lang="scss">
+<style lang="scss">
   @import 'assets/scss/reset.css';
   @import 'assets/scss/design.scss';
-  </style>
+
+  #app
+  {
+    overflow-x: hidden;
+  }
+
+</style>
